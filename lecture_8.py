b1e985e829b76094de1f5987a97bd238bb287e3b
jrfraser <64986011+jrfraser@users.noreply.github.com>
2020-05-23 12:56:34 -0700

    Add files via upload

---
+++ lecture_8.py
@@ -0,0 +1,18 @@
+########## Read in Data ##########
+#Open File
+filename = input("Enter the name of the data file: ")
+infile = open(filename, 'r')
+#Read lines from File
+datalist = []
+for line in infile:
+    #get data from line
+    date, h, l, r = (line.split(','))
+    lowtemp = int(l)
+    hightemp = int(h)
+    rainfall
+    month  = int(d)
+
+    #Put data into list
+#Close File
+########## Analyze Data ##########
+########## Present Results ##########
